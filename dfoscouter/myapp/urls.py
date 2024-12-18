546be9df2807a59f5b99f89bcdc3ea9498dce70b
Samuel Kim <samkunkim@gmail.com>
2024-12-17 23:54:37 -0800

    Working API call for base character

--- dfoscouter/myapp/urls.py
+++ dfoscouter/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_data
+from .views import get_data, get_characters
 
 urlpatterns = [
     path('api/data/', get_data, name='get_data'),
+    path('api/getchar/<str:characterName>', get_characters, name='get_characters'),
 ]
--- dfoscouter/myapp/views.py
+++ dfoscouter/myapp/views.py
@@ -1,7 +1,25 @@
+import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import JsonResponse
+from django.conf import settings
 
 @api_view(["GET"])
 def get_data(request):
     data = {"message": "Hello, World!"}
     return Response(data)
+
+def get_characters(request, characterName):
+    serverId = request.GET.get('serverId', 'all')
+    jobId = request.GET.get('jobId', '')
+    jobGrowId = request.GET.get('jobGrowId', '')
+    limit = request.GET.get('limit', '')
+    wordType = request.GET.get('wordType', '')
+    apikey = settings.API_KEY 
+
+    url = f"https://api.dfoneople.com/df/servers/{serverId}/characters?characterName={characterName}&jobId={jobId}&jobGrowId={jobGrowId}&limit={limit}&wordType={wordType}&apikey={apikey}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    return JsonResponse(data)
